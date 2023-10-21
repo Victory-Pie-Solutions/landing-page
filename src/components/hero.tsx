dd8d946d36d95dd529485aa78c23675c93a23228
salehghraiyib <saleh.ghrayeb@gmail.com>
2023-10-21 15:57:04 +0200

    components added

---
+++ src/components/hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+  return (
+<section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+    <div className="text-center lg:w-2/3 w-full">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Planspiel </h1>
+      <p className="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default Hero
