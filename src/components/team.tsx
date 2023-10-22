4aa62490161830d679c90fd979f3f19a3a4ffb59
salehghraiyib <saleh.ghrayeb@gmail.com>
2023-10-22 20:35:38 +0200

    added team members pt1

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import saleh from '../imgs/saleh.jpg'
+import patboke from '../imgs/patboke.png'
+import natnicha from '../imgs/natnicha.png'
 
 function Team() {
   return (
@@ -31,18 +33,18 @@ function Team() {
       </div>
       <div className="p-4 lg:w-1/2">
         <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-          <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/204x204" />
+          <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={natnicha.src} />
           <div className="flex-grow sm:pl-8">
-            <h2 className="title-font font-medium text-lg text-gray-900">Atticus Finch</h2>
+            <h2 className="title-font font-medium text-lg text-gray-900">Natnicha Rodtong</h2>
             <h3 className="text-gray-500 mb-3">UI Developer</h3>
           </div>
         </div>
       </div>
       <div className="p-4 lg:w-1/2">
         <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-          <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/206x206" />
+          <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={patboke.src} />
           <div className="flex-grow sm:pl-8">
-            <h2 className="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
+            <h2 className="title-font font-medium text-lg text-gray-900">Patboke Jitthamma</h2>
             <h3 className="text-gray-500 mb-3">Designer</h3>
           </div>
         </div>
