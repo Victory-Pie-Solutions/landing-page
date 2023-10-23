8b69c84b7705c3d544e706e4e14a9cc55cf1e70d
EunYoung Choi <youngchoi2094@gmail.com>
2023-10-23 12:53:22 +0200

    add mailto func

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,48 +1,79 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
     <section id="contact" className="text-gray-600 body-font relative">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-col text-center w-full mb-12">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-      <p className="lg:w-1/3 mx-auto leading-relaxed text-base">
-We value your input and are eager to hear from you. Whether you have questions, feedback, or simply want to reach out, don't hesitate to get in touch with us</p>
-    </div>
-    <div className="lg:w-1/2 md:w-2/3 mx-auto">
-      <div className="flex flex-wrap -m-2">
-        <div className="p-2 w-1/2">
-          <div className="relative">
-            <label className="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-          </div>
-        </div>
-        <div className="p-2 w-1/2">
-          <div className="relative">
-            <label className="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-          </div>
-        </div>
-        <div className="p-2 w-full">
-          <div className="relative">
-            <label className="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-          </div>
-        </div>
-        <div className="p-2 w-full">
-          <button className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Send</button>
-        </div>
-        <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-          <a className="text-blue-500">victorypiesolutions@outlook.com</a>
-          <p className="leading-normal my-5"> Reichenhainer Straße
-            <br />Chemnitz Germany
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-12">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            Contact Us
+          </h1>
+          <p className="lg:w-1/3 mx-auto leading-relaxed text-base">
+            We value your input and are eager to hear from you. Whether you have
+            questions, feedback, or simply want to reach out, don't hesitate to
+            get in touch with us
           </p>
         </div>
+        <div className="lg:w-1/2 md:w-2/3 mx-auto">
+          <div className="flex flex-wrap -m-2">
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                ></textarea>
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <button className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                Send
+              </button>
+            </div>
+            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+              <a
+                className="text-blue-500"
+                href="mailto:victorypiesolutions@outlook.com"
+              >
+                victorypiesolutions@outlook.com
+              </a>
+              <p className="leading-normal my-5">
+                {" "}
+                Reichenhainer Straße
+                <br />
+                Chemnitz Germany
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
