6b209ac39f441c89a6e1de27dbbf01f07ef73a94
salehghraiyib <saleh.ghrayeb@gmail.com>
2023-10-23 10:03:24 +0200

    added more team pic

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -3,6 +3,7 @@ import saleh from '../imgs/saleh.jpg'
 import patboke from '../imgs/patboke.png'
 import natnicha from '../imgs/fern.png'
 import emily from '../imgs/emily.png'
+import sassan from '../imgs/sassan.png'
 
 function Team() {
   return (
@@ -52,9 +53,9 @@ function Team() {
       </div>
       <div className="p-4 lg:w-1/2">
         <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-          <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/206x206" />
+          <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={sassan.src} />
           <div className="flex-grow sm:pl-8">
-            <h2 className="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
+            <h2 className="title-font font-medium text-lg text-gray-900">Sasan Chitsazi</h2>
             <h3 className="text-gray-500 mb-3">Designer</h3>
           </div>
         </div>
