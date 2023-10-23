cc70deeaa750a624928dd92bc214061db39230f0
salehghraiyib <saleh.ghrayeb@gmail.com>
2023-10-23 10:56:59 +0200

    added logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
+import logo from '@/imgs/logo.png'
 
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span className="ml-3 text-xl">VPS</span>
+      <img className="w-40 h-15 text-white" src={logo.src}/>
     </a>
     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Victory Pie Solutions
     </p>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '@/imgs/logo.png'
 
 export const Header = () => {
 
@@ -7,10 +8,7 @@ export const Header = () => {
     <header className={`sticky top-0 z-50 transition-shadow shadow bg-opacity-80 backdrop-blur-lg backdrop-filter"`}>
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span className="ml-3 text-xl">VPS</span>
+      <img className="w-40 h-15 text-white" src={logo.src}/>
     </a>
     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
       <a className="mr-5 hover:text-gray-900" href='#hero'>Home</a>
