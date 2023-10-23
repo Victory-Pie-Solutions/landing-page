99e447d79ef43cda369a4f1517c017315e092090
EunYoung Choi <youngchoi2094@gmail.com>
2023-10-23 14:25:23 +0200

    Edited home id name

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import logo from '@/imgs/logo.png'
+import React from "react";
+import logo from "@/imgs/logo.png";
 
 export const Header = () => {
-
-
   return (
-    <header className={`sticky top-0 z-50 transition-shadow shadow bg-opacity-80 backdrop-blur-lg backdrop-filter"`}>
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <img className="w-40 h-15 text-white" src={logo.src}/>
-    </a>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900" href='#hero'>Home</a>
-      <a className="mr-5 hover:text-gray-900" href='#vision'>Vision</a>
-      <a className="mr-5 hover:text-gray-900" href='#team'>Team</a>
-      <a className="mr-5 hover:text-gray-900" href='#contact'>Contact</a>
-    </nav>
-  </div>
-</header>
-  )
-}
+    <header
+      className={`sticky top-0 z-50 transition-shadow shadow bg-opacity-80 backdrop-blur-lg backdrop-filter"`}
+    >
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <img className="w-40 h-15 text-white" src={logo.src} />
+        </a>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900" href="#home">
+            Home
+          </a>
+          <a className="mr-5 hover:text-gray-900" href="#vision">
+            Vision
+          </a>
+          <a className="mr-5 hover:text-gray-900" href="#team">
+            Team
+          </a>
+          <a className="mr-5 hover:text-gray-900" href="#contact">
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import img from '../imgs/pie.jpg'
+import React from "react";
+import img from "../imgs/pie.jpg";
 function Hero() {
   return (
-<section className="text-gray-600 body-font" id="hero">
-  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={img.src} />
-    <div className="text-center lg:w-2/3 w-full">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Victory Pie Solutions </h1>
-      <p className="mb-8 leading-relaxed">A Slice of Trust, Innovation, and Comfort in Every Morning, Unlocking Your Industry's Full Potential</p>
-    </div>
-    <span className="inline-block h-1 w-10 rounded bg-orange-500 mt-8 mb-6"></span>
-  </div>
-  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-  <div className="text-center lg:w-2/3 w-full">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">About</h1>
-      <p className="mb-8 leading-relaxed">Victory Pie Solutions is a tech company in Chemnitz, Germany. We started in 2023 with the goal to offer better tech solutions. We focus on three things: making our services easy to use, solving problems for all types of customers using new technology, and mixing the best tech tools to make complete solutions. At VPS, we make it easy to fix your problems—just tell us what you need with one click, and we'll help you succeed</p>
-      <span className="inline-block h-1 w-10 rounded bg-orange-500 mt-8 mb-6"></span>
-    </div>
-  </div>    
-</section>
-  )
+    <section className="text-gray-600 body-font" id="home">
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <img
+          className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+          alt="hero"
+          src={img.src}
+        />
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            Victory Pie Solutions{" "}
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            A Slice of Trust, Innovation, and Comfort in Every Morning,
+            Unlocking Your Industry's Full Potential
+          </p>
+        </div>
+        <span className="inline-block h-1 w-10 rounded bg-orange-500 mt-8 mb-6"></span>
+      </div>
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            About
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Victory Pie Solutions is a tech company in Chemnitz, Germany. We
+            started in 2023 with the goal to offer better tech solutions. We
+            focus on three things: making our services easy to use, solving
+            problems for all types of customers using new technology, and mixing
+            the best tech tools to make complete solutions. At VPS, we make it
+            easy to fix your problems—just tell us what you need with one click,
+            and we'll help you succeed
+          </p>
+          <span className="inline-block h-1 w-10 rounded bg-orange-500 mt-8 mb-6"></span>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
