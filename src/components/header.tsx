7c852472f10d8bba21b4017a4c0c8b427c33c421
EunYoung Choi <youngchoi2094@gmail.com>
2023-10-24 11:42:36 +0200

    Added header hover color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,20 +7,23 @@ export const Header = () => {
       className={`sticky top-0 z-50 transition-shadow shadow bg-opacity-80 backdrop-blur-lg backdrop-filter"`}
     >
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-cefvnter">
-        <a className="flex title-font font-medium items-center text-gray-600 mb-4 md:mb-0">
+        <a
+          className="flex title-font font-medium items-center text-gray-600 mb-4 md:mb-0"
+          href="#home"
+        >
           <img className="w-40 h-15 text-white" src={logo.src} />
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900" href="#home">
+          <a className="mr-5 hover:text-orange-500" href="#home">
             Home
           </a>
-          <a className="mr-5 hover:text-gray-900" href="#vision">
+          <a className="mr-5 hover:text-orange-500" href="#vision">
             Vision
           </a>
-          <a className="mr-5 hover:text-gray-900" href="#team">
+          <a className="mr-5 hover:text-orange-500" href="#team">
             Team
           </a>
-          <a className="mr-5 hover:text-gray-900" href="#contact">
+          <a className="mr-5 hover:text-orange-500" href="#contact">
             Contact
           </a>
         </nav>
