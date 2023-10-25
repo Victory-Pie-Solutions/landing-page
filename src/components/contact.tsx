a28eed1594abef3955e10fa897f7cd708bbef003
Natnicha <nat.rodtong@gmail.com>
2023-10-25 21:01:38 +0200

    feat: add function send email to a button in Contact Us

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from 'emailjs-com';
+
 
 function Contact() {
+  const YOUR_SERVICE_ID = 'service_lvq1sbd'
+  const YOUR_TEMPLATE_ID = 'template_sbeqm4p'
+  const YOUR_USER_ID = 'aRkPlz6ncD2fBKbz_'
+
+  const sendEmail = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    const form = document.getElementById('form') as HTMLFormElement;
+    emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form, YOUR_USER_ID)
+      .then((result) => {
+        window.location.reload()
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+  
   return (
     <section id="contact" className="text-gray-600 body-font relative">
+      <form  id="form"  onSubmit={(e) => sendEmail}>
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-12">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-600">
@@ -22,7 +41,7 @@ function Contact() {
                 <input
                   type="text"
                   id="name"
-                  name="name"
+                  name="from_name"
                   className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
               </div>
@@ -33,9 +52,21 @@ function Contact() {
                 <input
                   type="email"
                   id="email"
-                  name="email"
+                  name="from_email"
                   className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <div className="relative">
+                <label className="leading-7 text-sm text-gray-600">
+                  Subject
+                </label>
+                <input
+                  id="subject"
+                  name="subject"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                ></input>
               </div>
             </div>
             <div className="p-2 w-full">
@@ -51,7 +82,9 @@ function Contact() {
               </div>
             </div>
             <div className="p-2 w-full">
-              <button className="flex mx-auto text-white bg-orange-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+              <button className="flex mx-auto text-white bg-orange-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
+              onClick={(event) => sendEmail(event)}
+              >
                 Send
               </button>
             </div>
@@ -72,6 +105,7 @@ function Contact() {
           </div>
         </div>
       </div>
+      </form>
     </section>
   );
 }
