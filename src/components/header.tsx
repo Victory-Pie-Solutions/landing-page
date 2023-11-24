2eecd0af586ab90d830abd2d4e4e7fca3f7cb1d4
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2023-11-24 15:59:16 +0100

    feat: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,49 @@
 import React from "react";
 import logo from "@/imgs/logo.png";
 
-export const Header = ({showDecleration}) => {
+export const Header = ({ showDecleration }) => {
   return (
     <header
       className={`sticky top-0 z-50 transition-shadow shadow bg-opacity-80 backdrop-blur-lg backdrop-filter"`}
     >
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-cefvnter" 
-        style={{
-          paddingTop:"40px"
-        }}>
+      <div className="container mx-auto flex flex-wrap px-5 py-8 flex-col md:flex-row">
         <a
           className="flex title-font font-medium items-center text-gray-600 mb-4 md:mb-0"
-          href="#home" onClick={() => showDecleration(0)}
+          href="#home"
+          onClick={() => showDecleration(0)}
         >
-          <img className="w-40 h-15 text-white position: absolute" src={logo.src} />
+          <img
+            className="md:justify-center w-40 h-15 text-white absolute "
+            src={logo.src}
+          />
         </a>
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center"
-          style={{
-            position:"relative", 
-            paddingTop:"20px"
-          }}>
-          <a className="mr-5 hover:text-orange-500" onClick={() => showDecleration(0)} href="#home">
+        <nav className="md:mx-auto flex flex-wrap items-center text-base justify-center">
+          <a
+            className="mx-3 hover:text-orange-500"
+            onClick={() => showDecleration(0)}
+            href="#home"
+          >
             Home
           </a>
-          <a className="mr-5 hover:text-orange-500" onClick={() => showDecleration(0)} href="#vision">
+          <a
+            className="mx-3 hover:text-orange-500"
+            onClick={() => showDecleration(0)}
+            href="#vision"
+          >
             Vision
           </a>
-          <a className="mr-5 hover:text-orange-500" onClick={() => showDecleration(0)} href="#team">
+          <a
+            className="mx-3 hover:text-orange-500"
+            onClick={() => showDecleration(0)}
+            href="#team"
+          >
             Team
           </a>
-          <a className="mr-5 hover:text-orange-500" onClick={() => showDecleration(0)} href="#contact">
+          <a
+            className="mx-3 hover:text-orange-500"
+            onClick={() => showDecleration(0)}
+            href="#contact"
+          >
             Contact
           </a>
         </nav>
