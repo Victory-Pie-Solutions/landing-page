68f1a7c02f901a3f2c5150acec547e0319c6e2f8
salehghraiyib <saleh.ghrayeb@gmail.com>
2024-02-05 14:33:23 +0100

    Added Update

--- src/components/current.tsx
+++ src/components/current.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import across from '@/imgs/across.png'
+import group from '@/imgs/group.jpeg'
 
 function Current() {
   return (
     <section id="current" className="text-gray-600 body-font">
+          <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-600">Pitch 3 - Product Demo</h1>
+        <p className="mb-8 leading-relaxed">
+         
+The previous week, we presented our Across module catalogue to customers, spotlighting the initial product and illustrating its advancements from both technical and business perspectives. We received valuable feedback from customers, providing insights into how we can enhance and further develop in subsequent phases. We are eager to unveil our final product in the upcoming pitch.
+          </p>
+          <br></br>
+          <img className="w-80% h-80%" style={{display:"inline"}}
+            src={group.src}
+            />
+          <br></br>
+    </div>
+    <div className="container px-5 py-24 mx-auto"></div>
   <div className="container px-5 py-24 mx-auto">
     <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-600">Current Project</h1>
